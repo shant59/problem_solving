dd5a97a941aa969d39222253734da4f35cd5005c
shant59 <2021331059@student.sust.edu>
2023-12-26 12:20:39 +0600

    Adding File

--- Project_by_C++.cpp
+++ Project_by_C++.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 int main()
 {
-    cout<<"hello!!"<<endl;
+    ofstream txt("sample.txt");
+    cout<<"Enter your name";
+    string name;
+    cin>>name;
+    txt<<"My name is"+name;
     return 0;
 }
